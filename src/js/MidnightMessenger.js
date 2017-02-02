75348efa8f1320f93ccb773308d590e44571e537
Chris Tieman <christopher.tieman@egov.com>
2017-02-02 00:06:28 -0500

    Cleared out unneeded style injections, saved MidnightMessenger class for the time being

--- src/js/MidnightMessenger.js
+++ src/js/MidnightMessenger.js
@@ -1,32 +1,9 @@
 class MidnightMessenger {
-  constructor(stylesheets) {
-    this.stylesheets = stylesheets;
+  constructor() {
+
   }
 
-  injectStyles() {
-    const style = document.createElement('style');
-
-    this.stylesheets.forEach(stylesheet => {
-      if (stylesheet.enabled && !stylesheet.enabled()) {
-        return;
-      }
-
-      const node = style.cloneNode();
-      node.type = 'text/css';
-
-      if (node.styleSheet) {
-        node.styleSheet.cssText = stylesheet.styleText;
-      } else {
-        node.appendChild(document.createTextNode(stylesheet.styleText));
-      }
-
-      document.body.appendChild(node);
-    });
-  }
-
-  init() {
-    this.injectStyles();
-  }
+  init() {}
 }
 
 export default MidnightMessenger;
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,6 @@
-import MidnightMessenger from './MidnightMessenger';
+// import MidnightMessenger from './MidnightMessenger';
 
-import defaultStylesheet from '../styles/midnight-messenger.scss';
+import '../styles/midnight-messenger.scss';
 
-// Stylesheets
-const STYLESHEETS = [
-  {
-    id: 'midnight-messenger',
-    styleText: defaultStylesheet,
-  },
-];
-
-const app = new MidnightMessenger(STYLESHEETS);
-app.init();
+// const app = new MidnightMessenger(STYLESHEETS);
+// app.init();
