d0239b2e3597abc60a45d3fb48edd78cd43702a3
Chris Tieman <christopher.tieman@egov.com>
2017-02-01 22:11:27 -0500

    Switched from personal build to webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    filename: 'dist/js/midnight-messenger.js',
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './manifest.json', to: 'dist/manifest.json' },
+      { from: './icon*.png', to: 'dist' },
+    ]),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.es6'],
+  },
+  watch: true,
+};
